3c8dd1a6962acb0ff570dfc4305e0ecd99033510
JSmith604 <jacquelinesmith190@gmail.com>
2021-01-14 07:48:42 +0000

    updated fetcher.js

--- fetcher.js
+++ fetcher.js
@@ -3,20 +3,18 @@ const url = process.argv[2];
 const path = process.argv[3];
 const request = require('request');
 const fs = require('fs');
-// fs.open(path, flags[mode], callback)
-
 const options = {
-    url: url,
-    method: 'GET'
+  url: url,
+  method: 'GET'
 };
 
 request(options, (err, res, body) => {
-    if (err) {
-        return console.log(err);
-    }
-     console.log(fs.writeFile(path, body, (err) => {
-       if (err) throw err;
-     }))
+  if (err) {
+    return console.log(err);
+  }
+  console.log(fs.writeFile(path, body, (err) => {
+    if (err) throw err;
+  }));
 });
 
 
